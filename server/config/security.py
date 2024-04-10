0c663cb696e18c5f880d52b6f99a3b07323a0598
Vitaly Kotov <catvitalio@gmail.com>
2024-04-10 16:28:55 +0700

    Simplify security

--- server/config/security.py
+++ server/config/security.py
@@ -11,9 +11,7 @@ from .settings import settings
 access_security = JwtAccessBearerCookie(
     secret_key=settings.SECRET_KEY.get_secret_value(),
     access_expires_delta=timedelta(minutes=30),
-)
-refresh_security = JwtRefreshBearerCookie(
-    secret_key=settings.SECRET_KEY.get_secret_value(),
     refresh_expires_delta=timedelta(days=30),
 )
+refresh_security = JwtRefreshBearerCookie.from_other(access_security)
 hasher = CryptContext(schemes=['bcrypt'], deprecated='auto')
