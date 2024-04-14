a9d937baeb0158c0463d293373d5564421d1e322
Vitaly Kotov <catvitalio@gmail.com>
2024-04-15 04:07:27 +0700

    Add id as mapped_column

--- server/src/common/models/base.py
+++ server/src/common/models/base.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Integer
 from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, mapped_column
 
 
 class Base(DeclarativeBase):
-    id = Column(Integer, primary_key=True, index=True)  # noqa: A003
+    id = mapped_column(Integer, primary_key=True, index=True)  # noqa: A003
     __name__: str  # noqa: A003
 
     @declared_attr  # type: ignore
